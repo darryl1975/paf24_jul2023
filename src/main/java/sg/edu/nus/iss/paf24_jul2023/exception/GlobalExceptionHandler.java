d16a1da41df2ec842828efdf31aafa8d15127b7c
Darryl Ng <darryl1975@hotmail.com>
2023-06-26 14:22:31 +0800

    custom exception explanation and different alternative

--- src/main/java/sg/edu/nus/iss/paf24_jul2023/exception/GlobalExceptionHandler.java
+++ src/main/java/sg/edu/nus/iss/paf24_jul2023/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package sg.edu.nus.iss.paf24_jul2023.exception;
 
 import java.util.Date;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,9 +11,24 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    
+
+    // @ExceptionHandler(BankAccountNotFoundException.class)
+    // public ResponseEntity<ErrorMessage>
+    // handleBankAccountNotFoundException(BankAccountNotFoundException ex,
+    // HttpServletRequest request) {
+    // // forming the custom error message
+    // ErrorMessage errMsg = new ErrorMessage();
+    // errMsg.setStatusCode(404);
+    // errMsg.setTimeStamp(new Date());
+    // errMsg.setMessage("Bank account doesn't exist or have not been created.");
+    // errMsg.setDescription(request.getRequestURI());
+
+    // return ResponseEntity.ok().body(errMsg);
+    // }
+
     @ExceptionHandler(BankAccountNotFoundException.class)
-    public ModelAndView handleBankAccountNotFoundException(BankAccountNotFoundException ex, HttpServletRequest request) {
+    public ModelAndView handleBankAccountNotFoundException(BankAccountNotFoundException ex,
+            HttpServletRequest request) {
         // forming the custom error message
         ErrorMessage errMsg = new ErrorMessage();
         errMsg.setStatusCode(404);
